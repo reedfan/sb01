bccc4bb7ae216423110f1400d2c3f54476b6fcf6
reedfan <1617950759@qq.com>
2019-01-04 00:29:34 +0800

    swagger

--- src/main/java/com/ustc/reed/Sb01Application.java
+++ src/main/java/com/ustc/reed/Sb01Application.java
@@ -3,9 +3,11 @@ package com.ustc.reed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class Sb01Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/ustc/reed/controller/DeptController.java
+++ src/main/java/com/ustc/reed/controller/DeptController.java
@@ -3,7 +3,9 @@ package com.ustc.reed.controller;
 
 import com.ustc.reed.pojo.Dept;
 import com.ustc.reed.service.DeptService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,8 @@ public class DeptController {
     private DeptService deptService;
 
 
-    @RequestMapping("/findAll")
+    @GetMapping(value = "/findAll")
+    @ApiOperation(value = "查询部门所有信息")
     public List<Dept> findAll(){
         return deptService.findAll();
     }
