cb2106ebc10ae1dea82aa720a8f709861c6f3f3b
reedfan <1617950759@qq.com>
2019-01-06 22:40:02 +0800

    mockMVC

---
+++ src/test/java/com/ustc/reed/controller/DeptControllerTest.java
@@ -0,0 +1,50 @@
+package com.ustc.reed.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * created by reedfan on 2019/1/6
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeptControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void whenQuerySuccess() throws Exception {
+        String result = mockMvc.perform(
+                MockMvcRequestBuilders.
+                get("/dept/findAll")
+                        // .param("page", "3")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(7))
+                .andReturn().getResponse().getContentAsString();
+
+        System.out.println(result);
+    }
+
+
+}
+
+
